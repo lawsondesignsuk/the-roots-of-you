d74e2c3aa443b9aec709620ab3b37e13cff41fd3
lawsondesignsuk <lawsondesignsuk@gmail.com>
2025-06-12 21:16:03 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.booking-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    // Handle form submission, e.g., display a confirmation message
-    alert('Thank you for booking! You will receive a confirmation email shortly.');
-    
-    // Reset the form
-    this.reset();
-});
